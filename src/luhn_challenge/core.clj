527682078fa3a399f81cbce1789b5a94cb548bdb
denis <denis@vbox-ubuntu-11.04-32b>
2011-11-23 08:39:04 +0100

    implementing anon-chunk

--- src/luhn_challenge/core.clj
+++ src/luhn_challenge/core.clj
@@ -8,7 +8,8 @@
 
 (println "--------- BEGIN CORE  ----------" (java.util.Date.))
 
-(unfinished anon-chunk split-by-candidate split-by-non-candidate )
+(unfinished anon-map anon-map as-combi-map luhn? max-size-cb
+            min-size-cb )
 
 (defn digit?
   [c]
@@ -21,21 +22,67 @@
   (digit? \0) => true
   (digit? \9) => true)
 
-(defn anon-chunk-then-split "given a seq starting with a candidate and a reminder, returns: [a finite seq of anonymised data, a reminder of digits, a lazy seq of the rest]"
+(defn anon-chunk-then-split "given a seq starting with a candidate and a reminder : a seq of pair [digit, toAnon?], returns: a finite seq of anonymised data, a map {:rem [] :to-anon []} reminder of digits, a lazy seq of the rest]"
   [s])
+
+(defn anon-char
+  [])
+
+(defn anon-chunk "Given a seq of pair [digit, to-anon?] returns the anonymised seq"
+  [s] (let [ac (anon-char)]
+        (reduce (fn [r i] (assoc r i ac))
+                s
+                (reduce concat
+                        (map anon-map
+                             (as-combi-map s (min-size-cb) (max-size-cb)))))))
+
+(fact "anon-chunk"
+      (anon-chunk [:a :b :c]) => [:X :X :c]
+      (provided
+       (min-size-cb) => :min
+       (max-size-cb) => :max
+       (anon-char) => :X
+       (as-combi-mapy [:a :b :c]  :min :max) => [:map1 :map2]
+       (anon-map :map1) => [0 1]
+       (anon-map :map2) => [0]))
+
+(future-fact "anon-chunk: empty seq"
+      (anon-chunk []) => [])
+
+(future-fact "anon-chunk: not a credit card number"
+             (anon-chunk [:a :b :c :d :e :f]) => [:a :b :c :d :e :f]
+             (provided
+              (min-size-cb) => 2
+              (max-size-cb) => 4
+              (luhn? [:a :b]) => false
+              (luhn? [:b :c]) => false
+              (luhn? [:c :d]) => false
+              (luhn? [:d :e]) => false
+              (luhn? [:e :f]) => false
+              (luhn? [:a :b :c]) => false
+              (luhn? [:b :c :d]) => false
+              (luhn? [:c :d :e]) => false
+              (luhn? [:a :b :c :d]) => false
+              (luhn? [:b :c :d :e]) => false
+              (luhn? [:c :d :e :f]) => false))
 
 (defn anon "Takes a seq of char, return a seq of char anonymised"
   ([s] (anon [] (first s) (next s) 0))
-  ([f to-eat acc is cnt] (lazy-seq (cons f
-                                         (do #_(println "f=" f "to-eat=" to-eat "acc=" acc "is=" is)
-                                             (cond (< 4 cnt)    nil
-                                                   (seq to-eat) (anon (first to-eat) (next to-eat)    acc is        (inc cnt))
-                                                   (seq is)     (if (digit? (first is))
-                                                                  (let [[anonized azz rem-seq] (anon-chunk-then-split is acc)]
-                                                                    (anon (first anonized) (next anonized) azz rem-seq (inc cnt)))
-                                                                  (anon (first is) (anon-chunk acc) [] (next is) (inc cnt)))
-                                                   (seq acc)    :shouldnt-be-there-yet
-                                                   :else        nil))))))
+  ([f to-eat acc is cnt]
+     (lazy-seq
+      (cons f
+            (cond (< 4 cnt)    nil
+                  (seq to-eat) (anon (first to-eat) (next to-eat)    acc is        (inc cnt))
+                  (seq is)     (if (digit? (first is)) (let [[anonized azz rem-seq] (anon-chunk-then-split is acc)]
+                                                         (anon (first anonized) (next anonized) azz rem-seq (inc cnt)))
+                                   (anon (first is) (anon-chunk acc) [] (next is) (inc cnt)))
+                  (seq acc)    (let [[ff & rr] (anon-chunk acc)] (anon ff rr [] is (inc cnt)))
+                  :else        nil)))))
+
+(fact "anon: end, but acc remaining"
+      (anon :f [] [:ac1 :ac2] [] 0)      => [:f :anon1 :anon2]
+      (provided
+       (anon-chunk [:ac1 :ac2]) => [:anon1 :anon2]))
 
 (fact "anon: consumation in progress, then eof and acc empty"
       (anon \a [\b] [] [] 0) => [\a \b])
@@ -45,12 +92,6 @@
       (provided
        (digit? \b)     => false
        (anon-chunk []) => []))
-
-(fact "anon: test call to anon-chunk-then-split and transmit"
-      (anon \Y [] [\1 \2] [\3 \4] 0)      => [\Y \1 \X \3 \X]
-      (provided
-       (digit? \3)                             => true
-       (anon-chunk-then-split [\3 \4] [\1 \2]) => [[\1 \X \3 \X] [] []]))
 
 (fact "anon: test call to anon-chunk-then-split and transmit"
       (anon :f [] [:ac1 :ac2] [:is1 :is2] 0)      => [:f :anon1 :anon2]
