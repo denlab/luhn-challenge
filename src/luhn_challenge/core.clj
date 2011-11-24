70f91189e1eec453fdc54b511119c2548177dcef
denis <denis@vbox-ubuntu-11.04-32b>
2011-11-24 09:14:03 +0100

    luhn? done

--- src/luhn_challenge/core.clj
+++ src/luhn_challenge/core.clj
@@ -1,5 +1,5 @@
 (ns ^{:doc "Luhn challenge by crazybob: http://blog.crazybob.org/2011/11/coding-challenge-luhny-bin.html"}
-  luhn-challenge.core
+  map-luhn?-challenge.core
   (:use     [midje.sweet])
   (:use     [clojure.pprint :only [pp pprint]])
   (:require [clojure.walk :as w])
@@ -8,7 +8,15 @@
 
 (println "--------- BEGIN CORE  ----------" (java.util.Date.))
 
-(unfinished anon-map   luhn?)
+(unfinished )
+
+(comment (defn luhn?
+   [s] ((reverse s))))
+
+(comment (fact "luhn?"
+       (luhn? ['5 '6 '7 '8]) => true))
+
+(fact "")
 
 (defn digit?
   [c]
@@ -33,16 +41,94 @@
 (defn max-size-cb
   [] 16)
 
+(defn vec-to-map
+  [v] (zipmap (range (count v))
+              v))
+
+(fact "vec-to-map"
+      (vec-to-map [:a :b]) => {0 :a 1 :b})
+
 (defn as-combi-map "Takes a vec and returns all combination of min max"
   [min max s]
-  (mapcat (fn [size] (map (fn [start] (subvec s start (+ start size)))
-                         (range (- (inc (count s)) size))))
-          (range min (inc max))))
+  (let [s-size (inc (count s))]
+    (mapcat (fn [size] (map (fn [start] (vec-to-map (subvec s start (+ start size))))
+                           (range (- s-size size))))
+            (range min (inc max)))))
 
-(fact "as-combi-map"   ;; 0  1  2  3  4  5       
-      (as-combi-map 2 4 [:a :b :c :d :e :f]) => [{0 :a 1 :b} {1 :b 2 :c} {2 :c 3 :d} {3 :d 4 :e} {4 :e 5 :f}
-                                                 {0 :a 1 :b 2 :c} {1 :b 2 :c 3 :d} {2 :c 3 :d 4 :e} {3 :d 4 :e 5 :f}
-                                                 {0 :a 1 :b 2 :c 3 :d} {1 :b 2 :c 3 :d 4 :e} {2 :c 3 :d 4 :e 5 :f}])
+(fact "as-combi-map"
+      (as-combi-map 1 2 [:a :b :c]) => [:m1 :m2 :m3 :m4 :m5]
+      (provided
+       (vec-to-map [:a])    => :m1
+       (vec-to-map [:b])    => :m2
+       (vec-to-map [:c])    => :m3
+       (vec-to-map [:a :b]) => :m4
+       (vec-to-map [:b :c]) => :m5))
+
+(fact "as-combi-map: edge case empty seq"
+      (as-combi-map 1 2 []) => [])
+
+(defn char2int "Returns the digit represented by this char"
+  [c] (case c
+        \0 0 \5 5
+        \1 1 \6 6
+        \2 2 \7 7
+        \3 3 \8 8
+        \4 4 \9 9))
+
+(tabular
+ (fact (char2int ?c) => ?expected) 
+ ?c ?expected
+ \0 0 \5 5
+ \1 1 \6 6
+ \2 2 \7 7
+ \3 3 \8 8
+ \4 4 \9 9)
+
+
+(defn charx2 "Takes a char representing a digit, multiply this digit by 2, and returns all the digits of this number"
+  [c] (case c
+        \0 [0] \5 [1 0]
+        \1 [2] \6 [1 2]
+        \2 [4] \7 [1 4]
+        \3 [6] \8 [1 6]
+        \4 [8] \9 [1 8]))
+
+(tabular
+ (fact (charx2 ?c) => ?expected) 
+ ?c ?expected
+ \0 [0]        \5 [1 0]
+ \1 [2]        \6 [1 2]
+ \2 [4]        \7 [1 4]
+ \3 [6]        \8 [1 6]
+ \4 [8]        \9 [1 8])
+
+(defn map-luhn?
+  [s] (zero? (rem
+              (reduce + (mapcat (fn [[odd even]] (cons (char2int odd) (charx2 even)))
+                                (partition-all 2 (reverse s))))
+              10)))
+
+(fact "map-luhn?"
+      (map-luhn? [\5 \6 \7 \8]) => true)
+
+;; TODO delete me
+(future-fact "map-luhn?"
+      (map-luhn? {0 :a 1 :b}) => :result
+      (provided
+       (luhn? [:a :b]) => :result))
+
+(defn anon-map "Takes a map (which represent a vector of value) and return a seq of indice of vals to anonymise"
+  [m] (if (map-luhn? (vals m))
+        (keys m)
+        []))
+
+(fact "anon-map: map-luhn? test passes"
+      (anon-map {0 :a 1 :b} ) => [0 1]
+      (provided (map-luhn? [:a :b]) => true))
+
+(fact "anon-map: map-luhn? test don't passes"
+      (anon-map {0 :a 1 :b} ) => []
+      (provided (map-luhn? [:a :b]) => false))
 
 (defn anon-chunk "Given a seq of pair [digit, to-anon?] returns the anonymised seq"
   [s] (let [ac (anon-char)]
@@ -70,17 +156,17 @@
              (provided
               (min-size-cb) => 2
               (max-size-cb) => 4
-              (luhn? [:a :b]) => false
-              (luhn? [:b :c]) => false
-              (luhn? [:c :d]) => false
-              (luhn? [:d :e]) => false
-              (luhn? [:e :f]) => false
-              (luhn? [:a :b :c]) => false
-              (luhn? [:b :c :d]) => false
-              (luhn? [:c :d :e]) => false
-              (luhn? [:a :b :c :d]) => false
-              (luhn? [:b :c :d :e]) => false
-              (luhn? [:c :d :e :f]) => false))
+              (map-luhn? [:a :b]) => false
+              (map-luhn? [:b :c]) => false
+              (map-luhn? [:c :d]) => false
+              (map-luhn? [:d :e]) => false
+              (map-luhn? [:e :f]) => false
+              (map-luhn? [:a :b :c]) => false
+              (map-luhn? [:b :c :d]) => false
+              (map-luhn? [:c :d :e]) => false
+              (map-luhn? [:a :b :c :d]) => false
+              (map-luhn? [:b :c :d :e]) => false
+              (map-luhn? [:c :d :e :f]) => false))
 
 (defn anon "Takes a seq of char, return a seq of char anonymised"
   ([s] (anon [] (first s) (next s) 0))
