7101d1c064b69362d1873c1669e6289b3c7cdab4
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-11-27 19:08:12 +0100

    Change the next call by the rest one. The test are now green!

--- src/luhn_challenge/core.clj
+++ src/luhn_challenge/core.clj
@@ -17,17 +17,17 @@
   [s] (iterate (fn [{:keys [curr-char curr-vec curr-seq cnt]}]
                  (cond  (> cnt 50)    (/ 0 0)
                         (seq curr-vec) {:curr-char (first curr-vec)
-                                        :curr-vec  (next curr-vec)
+                                        :curr-vec  (rest curr-vec)
                                         :curr-seq  curr-seq
                                         :cnt       (inc cnt)}
                         (seq curr-seq) {:curr-char (first (first curr-seq))
-                                        :curr-vec  (next (first curr-seq))
-                                        :curr-seq  (next curr-seq)
+                                        :curr-vec  (rest (first curr-seq))
+                                        :curr-seq  (rest curr-seq)
                                         :cnt       (inc cnt)}
                         :else          {}))
                {:curr-char (first (first s))
-                :curr-vec  (next (first s))
-                :curr-seq  (next s)
+                :curr-vec  (rest (first s))
+                :curr-seq  (rest s)
                 :cnt       0}))
 
 (let [r (lazy-concat-helper- (iterate (fn [[a b]] [(inc a) (inc b)]) [1 2]))]
@@ -36,9 +36,8 @@
         (:curr-vec (first r))         => [2]
         (first (:curr-seq (first r))) => [2 3]))
 
-;.;. FAIL at (NO_SOURCE_FILE:1)
-;.;.     Expected: ({:curr-char 1, :curr-vec [2], :curr-seq [[3 4]], :cnt 0} {:curr-char 2, :curr-vec [], :curr-seq [[3 4]], :cnt 1} {:curr-char 3, :curr-vec [4], :curr-seq [], :cnt 2} {:curr-char 4, :curr-vec [], :curr-seq [], :cnt 3} {})
-;.;.       Actual: ({:curr-char 1, :curr-vec (2), :curr-seq ([3 4]), :cnt 0} {:curr-char 2, :curr-vec nil, :curr-seq ([3 4]), :cnt 1} {:curr-char 3, :curr-vec (4), :curr-seq nil, :cnt 2} {:curr-char 4, :curr-vec nil, :curr-seq nil, :cnt 3} {})
+;.;. The highest reward for a man's toil is not what he gets for it but
+;.;. what he becomes by it. -- Ruskin
 (fact
   (take 5 (lazy-concat-helper- [[1 2] [3 4]])) => '({:curr-char 1
                                                      :curr-vec  [2]
