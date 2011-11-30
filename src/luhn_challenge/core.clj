8354046028369486803898b80c0f0db8844a0104
denis <denis@vbox-ubuntu-11.04-32b>
2011-11-30 18:05:38 +0100

    in progress

--- src/luhn_challenge/core.clj
+++ src/luhn_challenge/core.clj
@@ -8,34 +8,91 @@
 
 (println "--------- BEGIN CORE  ----------" (java.util.Date.))
 
-(unfinished get-action blank? )
+(unfinished char-type digit? cc-max-size other-char? get-op get-action
+            blank? )
+
+(defn char-type "Given a char returns the type of it: :blank | :other | :digit"
+  [c])
+
+(defn next-op "Given a context returns a keyword representing the next operation to perform"
+  [{:keys [seq- digit-cnt blank-cnt to-eat]}]
+  (if to-eat
+    :consume
+    (case (char-type (first seq-))
+      :other :output-char-and-flush-acc
+      :digit (cond (< (inc digit-cnt) (cc-max-size)) :acc-digit
+                   :else                             :anon-chunk)
+      :blank (cond (zero? digit-cnt) :output-char-and-flush-acc
+                   (zero? blank-cnt) :init-count-blank
+                   :else             :acc-blank)
+      :empty :anon-chunk)))
+
+(fact "next-op : consuming"
+      (next-op {:to-eat :seq}) => :consume)
+
+(fact "next-op: other-char"
+      (next-op {:seq- [:c]}) => :output-char-and-flush-acc
+      (provided
+       (char-type :c) => :other))
+
+(fact "next-op: digit acc not full"
+      (next-op {:seq- [:c] :digit-cnt 0}) => :acc-digit
+      (provided
+       (cc-max-size)    => 2
+       (char-type :c) => :digit))
+
+(fact "next-op: digit acc full"
+      (next-op {:seq- [:c] :digit-cnt 1}) => :anon-chunk
+      (provided
+       (cc-max-size)    => 2
+       (char-type :c) => :digit))
+
+(fact "next-op: blank without digit acc"
+      (next-op {:seq- [:c] :digit-cnt 0}) => :output-char-and-flush-acc
+      (provided
+       (char-type :c) => :blank))
+
+(fact "next-op: blank with digit acc, no blank count"
+      (next-op {:seq- [:c] :digit-cnt 1 :blank-cnt 0}) => :init-count-blank
+      (provided
+       (char-type :c) => :blank))
+
+(fact "next-op: blank with digit acc, accumulating blank"
+      (next-op {:seq- [:c] :digit-cnt 1 :blank-cnt 1}) => :acc-blank
+      (provided
+       (char-type :c) => :blank))
+
+;; TODO redonant
+(fact "next-op: EOF, but to-eat"
+      (next-op {:seq- [] :to-eat [:x]}) => :consume)
+
+(fact "next-op: EOF, but has acc"
+      (next-op {:seq- [] :digit-cnt 1}) => :anon-chunk
+      (provided
+       (char-type nil) => :empty))
+
+
 
 (defn anon-proc-
-  [c ctx] )
+  [ctx] ((get-op (next-op ctx)) ctx))
 
-(future-fact "anon-proc-"
-      (anon-proc- :c :ctx) => 
-      (get-action :c :ctx) => :start-acc-char)
-
-
+(fact "anon-proc-"
+      (anon-proc- :ctx) => :ctx2
+      (provided
+       (next-op :ctx) => :op-keyword
+       ;; emulating a mock that returns a mock
+       (get-op  :op-keyword) => {:ctx :ctx2}))
 
 (defn anon- "Takes a seq of char, return a seq of vec of anonymised chars"
   [s] (take-while identity
-                  (iterate (fn [[seq- ctx]] 
-                             (cond (nil? seq-)   nil
-                                   (empty? seq-) [nil         (anon-proc- nil          ctx)]
-                                   :else         [(rest seq-) (anon-proc- (first seq-) ctx)]))
-                           [s {}])))
+                  (rest (iterate anon-proc- {:seq- s}))))
 
-(fact "anon-: nominal"
-      (anon- [:a :b]) => [[[:a :b] {}]
-                          [[:b]    :ctx1]
-                          [[]      :ctx2]
-                          [nil      :ctx3]]
+(fact "anon-"
+      (take 10 (anon- :in-seq)) => [:ctx1 :ctx2]
       (provided
-       (anon-proc- :a  {})    => :ctx1
-       (anon-proc- :b  :ctx1) => :ctx2
-       (anon-proc- nil :ctx2) => :ctx3))
+       (anon-proc- {:seq- :in-seq}) => :ctx1
+       (anon-proc- :ctx1)   => :ctx2
+       (anon-proc- :ctx2)   => nil))
 
 (defn anon "Takes a seq of char, returns a seq of anonymised chars"
   [s] (flatten (anon- s)))
@@ -46,32 +103,4 @@
   (anon- :in-seq) => [[:x1 :x2] [:x3]]))
 
 (println "--------- END CORE  ----------" (java.util.Date.))
-
-(comment 
-
-  (defrecord Add2
-      Calculate
-    (do-calc [entity input]) => (+ 2 input))
-
-  (fact
-   (do-calc (Mult2. 3)) => 6
-   (do-calc (Add2. 3)) => 5))
-
-(defprotocol Calculate
-  (do-calc [entity input]))
-
-(defrecord Mult2 [input]
-  Calculate
-  (do-calc [entity input] (* 2 input)))
-
-(defrecord Add2 [input]
-  Calculate
-  (do-calc [entity input] (+ 2 input)))
-
-(fact
- (do-calc (Mult2. 0) 3) => 6)
-
-(fact
- (do-calc (Add2. 0) 3) => 5)
-
 
