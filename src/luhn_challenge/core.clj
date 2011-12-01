dcdea3bf8f76998f70f1027fb6c0b719064b6b2b
denis <denis@vbox-ubuntu-11.04-32b>
2011-12-01 08:28:39 +0100

    about to use protocols ;)

--- src/luhn_challenge/core.clj
+++ src/luhn_challenge/core.clj
@@ -8,66 +8,85 @@
 
 (println "--------- BEGIN CORE  ----------" (java.util.Date.))
 
-(unfinished char-type digit? cc-max-size other-char? get-op get-action
+(unfinished char-type digit? cc-max-size other-char?  get-action
             blank? )
 
+(defn k-output-char-and-flush-acc
+  [ctx] )
+
+(fact "k-output-char-and-flush-acc"
+      (k-output-char-and-flush-acc {:seq- [:c1 :c2], :acc :acc-seq}) => {:seq- [:c2], :to-eat :to-eat-seq})
+
+(let [kw->fn {:k-output-char-and-flush-acc k-output-char-and-flush-acc}]
+  (defn get-op
+   [op-keyw] (kw->fn op-keyw)))
+
+(fact
+ (get-op :k-output-char-and-flush-acc) => (exactly k-output-char-and-flush-acc))
+
 (defn char-type "Given a char returns the type of it: :blank | :other | :digit"
-  [c])
+  [c] (case c
+        \0 :digit, \1 :digit, \2 :digit, \3 :digit, \4 :digit, \5 :digit, \6 :digit, \7 :digit, \8 :digit, \9 :digit,
+        \- :blank, \space :blank,
+        :other))
+
+(tabular
+ (fact (char-type ?c) => ?expected)
+ ?c ?expected
+ \0 :digit, \1 :digit, \2 :digit, \3 :digit, \4 :digit, \5 :digit, \6 :digit, \7 :digit, \8 :digit, \9 :digit,
+ \- :blank, \space :blank,
+ \_ :other, \a :other)
 
 (defn next-op "Given a context returns a keyword representing the next operation to perform"
   [{:keys [seq- digit-cnt blank-cnt to-eat]}]
   (if to-eat
-    :consume
+    :k-consume
     (case (char-type (first seq-))
-      :other :output-char-and-flush-acc
-      :digit (cond (< (inc digit-cnt) (cc-max-size)) :acc-digit
-                   :else                             :anon-chunk)
-      :blank (cond (zero? digit-cnt) :output-char-and-flush-acc
-                   (zero? blank-cnt) :init-count-blank
-                   :else             :acc-blank)
-      :empty :anon-chunk)))
+      :other :k-output-char-and-flush-acc
+      :digit (cond (< (inc digit-cnt) (cc-max-size)) :k-acc-digit
+                   :else                             :k-anon-chunk)
+      :blank (cond (zero? digit-cnt) :k-output-char-and-flush-acc
+                   (zero? blank-cnt) :k-init-count-blank
+                   :else             :k-acc-blank)
+      :empty :k-anon-chunk)))
 
 (fact "next-op : consuming"
-      (next-op {:to-eat :seq}) => :consume)
+      (next-op {:to-eat :seq}) => :k-consume)
 
 (fact "next-op: other-char"
-      (next-op {:seq- [:c]}) => :output-char-and-flush-acc
+      (next-op {:seq- [:c]}) => :k-output-char-and-flush-acc
       (provided
        (char-type :c) => :other))
 
 (fact "next-op: digit acc not full"
-      (next-op {:seq- [:c] :digit-cnt 0}) => :acc-digit
+      (next-op {:seq- [:c] :digit-cnt 0}) => :k-acc-digit
       (provided
        (cc-max-size)    => 2
        (char-type :c) => :digit))
 
 (fact "next-op: digit acc full"
-      (next-op {:seq- [:c] :digit-cnt 1}) => :anon-chunk
+      (next-op {:seq- [:c] :digit-cnt 1}) => :k-anon-chunk
       (provided
        (cc-max-size)    => 2
        (char-type :c) => :digit))
 
 (fact "next-op: blank without digit acc"
-      (next-op {:seq- [:c] :digit-cnt 0}) => :output-char-and-flush-acc
+      (next-op {:seq- [:c] :digit-cnt 0}) => :k-output-char-and-flush-acc
       (provided
        (char-type :c) => :blank))
 
 (fact "next-op: blank with digit acc, no blank count"
-      (next-op {:seq- [:c] :digit-cnt 1 :blank-cnt 0}) => :init-count-blank
+      (next-op {:seq- [:c] :digit-cnt 1 :blank-cnt 0}) => :k-init-count-blank
       (provided
        (char-type :c) => :blank))
 
 (fact "next-op: blank with digit acc, accumulating blank"
-      (next-op {:seq- [:c] :digit-cnt 1 :blank-cnt 1}) => :acc-blank
+      (next-op {:seq- [:c] :digit-cnt 1 :blank-cnt 1}) => :k-acc-blank
       (provided
        (char-type :c) => :blank))
 
-;; TODO redonant
-(fact "next-op: EOF, but to-eat"
-      (next-op {:seq- [] :to-eat [:x]}) => :consume)
-
 (fact "next-op: EOF, but has acc"
-      (next-op {:seq- [] :digit-cnt 1}) => :anon-chunk
+      (next-op {:seq- [] :digit-cnt 1}) => :k-anon-chunk
       (provided
        (char-type nil) => :empty))
 
@@ -101,6 +120,14 @@
  (anon :in-seq) => [:x1 :x2 :x3]
  (provided
   (anon- :in-seq) => [[:x1 :x2] [:x3]]))
+
+(defprotocol State
+  (next [this]))
+
+(defrecord StateC [i]
+    State
+    (next [this] (println "i=" i ", C state")
+      ))
 
 (println "--------- END CORE  ----------" (java.util.Date.))
 
