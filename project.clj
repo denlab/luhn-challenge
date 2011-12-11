f18463cae19336dc085cc7ca257a246db5e0d9a3
Denis Labaye <denis.labaye@gmail.com>
2011-12-11 15:20:07 +0100

    removed unnecessary deps: clojure-contrib

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject luhn-challenge "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure         "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
                  [clj-http                    "0.1.3"]
                  [org.cloudhoist/stevedore    "0.5.0"]]
   :dev-dependencies [[midje "1.3.0-RC4"]
