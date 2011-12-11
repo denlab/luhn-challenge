f562a8984724ac95832fc6bd76ac15cb4063526c
Denis Labaye <denis.labaye@gmail.com>
2011-12-11 17:52:45 +0100

    removed lazytest deps that caused swank to fail

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "crazybob luhn challenge"
   :dependencies [[org.clojure/clojure  "1.3.0"]]
   :dev-dependencies [[midje                "1.3.0-RC4"]
-                     [lein-midje           "1.0.7"]
-                     [com.intelie/lazytest "1.0.0-SNAPSHOT"]])
+                     [lein-midje           "1.0.7"]])
 
 
