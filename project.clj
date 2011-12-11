a7048af46f2af4a84ba94c2551e7f28e6216b759
Denis Labaye <denis.labaye@gmail.com>
2011-12-11 15:29:49 +0100

    removed unnecessary repo

--- project.clj
+++ project.clj
@@ -3,8 +3,6 @@
   :dependencies [[org.clojure/clojure         "1.3.0"]]
   :dev-dependencies [[midje "1.3.0-RC4"]
                      [lein-midje "1.0.7"]
-                     [com.intelie/lazytest "1.0.0-SNAPSHOT"]]
-  :repositories {"sonatype"
-                 "http://oss.sonatype.org/content/repositories/releases"})
+                     [com.intelie/lazytest "1.0.0-SNAPSHOT"]])
 
 
