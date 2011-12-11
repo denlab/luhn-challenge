df40dcd4261189096f7d47f3774b2179f1c68b8b
Denis Labaye <denis.labaye@gmail.com>
2011-12-11 15:29:03 +0100

    upgrade to clojure 1.3.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject luhn-challenge "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure         "1.2.1"]]
+  :dependencies [[org.clojure/clojure         "1.3.0"]]
   :dev-dependencies [[midje "1.3.0-RC4"]
                      [lein-midje "1.0.7"]
                      [com.intelie/lazytest "1.0.0-SNAPSHOT"]]
