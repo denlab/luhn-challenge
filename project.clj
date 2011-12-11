1ecba1c5dff5b6426cb46d9dead91e1fe1a2f7cd
Denis Labaye <denis.labaye@gmail.com>
2011-12-11 15:21:49 +0100

    removed unnecessary deps: steavedore

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject luhn-challenge "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure         "1.2.1"]
-                 [clj-http                    "0.1.3"]
-                 [org.cloudhoist/stevedore    "0.5.0"]]
+                 [clj-http                    "0.1.3"]]
   :dev-dependencies [[midje "1.3.0-RC4"]
                      [lein-midje "1.0.7"]
                      [org.clojars.overtone/vijual "0.2.1"]
