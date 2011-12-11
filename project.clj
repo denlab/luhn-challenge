329509202d028e02d2d94ca1945ca8b381a3655a
Denis Labaye <denis.labaye@gmail.com>
2011-12-11 15:22:44 +0100

    removed unnecessary deps: clj/http

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject luhn-challenge "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure         "1.2.1"]
-                 [clj-http                    "0.1.3"]]
+  :dependencies [[org.clojure/clojure         "1.2.1"]]
   :dev-dependencies [[midje "1.3.0-RC4"]
                      [lein-midje "1.0.7"]
                      [org.clojars.overtone/vijual "0.2.1"]
